24ff8099cf270ed9728b8dc11fa58ff5a22bf46e
NicoHuelsmann <NicoHuelsmann@hotmail.com>
2025-09-30 10:26:05 +0200

    gitignore update

--- core/src/server.ts
+++ core/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import sqlite3 from "sqlite3";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 9000;
@@ -19,25 +20,31 @@ app.post("/echo", (req: Request, res: Response) => {
 });
 
 // Login
-app.get("/Login", (req: Request, res: Response) => {   
-    console.log ('test');
-  const db = new sqlite3.Database("core/Database/datenbank.db", (err) => {
-    if (err) {
-      console.error("Fehler beim Öffnen der Datenbank:", err.message);
-      res.status(500).json({ error: err.message });
-      return;
-    }
-  });
+app.post("/login", (req: Request, res: Response) => {
 
-  db.run('INSERT INTO tsta (STTXT) VALUES (?)', ["test"], (err) => {
-    if (err) {
-      console.error("Fehler beim Insert:", err.message);
-      res.status(500).json({ error: err.message });
-    } else {
-      res.json({ success: true });
-    }
-    db.close();
-  });
+    const db = new sqlite3.Database('/home/nico/Dokumente/ProjektBestellSystemITO1/core/database/datenbank.db', (err) => {
+      if (err) {
+        console.error("Fehler beim Öffnen der Datenbank:", err.message);
+        res.status(500).json({ error: err.message });
+        return;
+      }
+    });
+    
+    db.get(
+      'SELECT 1 FROM USR01 WHERE USRNAM = ? AND PWCODE = ?',
+      [req.body.username, req.body.password],
+      (err, row) => {
+        if (err) {
+          console.error("Fehler beim SELECT:", err.message);
+          res.status(500).json({ error: err.message });
+        } else if (row) {
+          res.json({ success: true, row });
+        } else {
+          res.json({ success: false, message: "Kein Benutzer gefunden" });
+        }
+        db.close();
+      }
+    );
 });
 
 app.listen(PORT, () => {
