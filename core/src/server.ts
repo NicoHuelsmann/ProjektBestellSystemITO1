8e799ecd746babfd48485628745932eece972ca4
NicoHuelsmann <NicoHuelsmann@hotmail.com>
2025-10-10 10:57:29 +0200

    Erste Datenbank Abfragen

--- core/src/server.ts
+++ core/src/server.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response } from "express";
+import express, { Request, response, Response } from "express";
 import cors from "cors";
-import sqlite3 from "sqlite3";
+import sqlite3, { Database } from "sqlite3";
 import path from "path";
 
 const app = express();
@@ -22,7 +22,33 @@ app.post("/echo", (req: Request, res: Response) => {
 // Login
 app.get("/users", (req: Request, res: Response) => {
   const db = new sqlite3.Database(
-    'C:\\Users\\nicoh\\Documents\\GitHub\\ProjektBestellSystemITO1\\core\\database\\datenbank.db',
+    '/home/nico/Dokumente/ProjektBestellSystemITO1/core/database/datenbank.db',
+    (err) => {
+      if (err) {
+        console.error(err.message);
+        res.status(500).json({ error: err.message });
+        return;
+      }
+    }
+  );
+  (async () => {
+    try {
+      const user = await fetchAll(db, `SELECT * FROM USR01`, []);
+      console.log(user);
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      db.close();
+    }
+  })();
+
+});
+
+// Role
+app.post("/role", (req: Request, res: Response) => {
+  const db = new sqlite3.Database(
+    '/home/nico/Dokumente/ProjektBestellSystemITO1/core/database/datenbank.db',
     (err) => {
       if (err) {
         console.error(err.message);
@@ -32,15 +58,42 @@ app.get("/users", (req: Request, res: Response) => {
     }
   );
 
-  db.all('SELECT * FROM USR01', [], (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-    } else {
-      res.json({ success: true, users: rows });
+  (async () => {
+    const db = new sqlite3.Database("/home/nico/Dokumente/ProjektBestellSystemITO1/core/database/datenbank.db");
+  
+    try {
+      console.log (req.query.persnr);
+      const user = await fetchFirst(db, `SELECT USR01.ROLEID FROM USR01 WHERE USR01.PERSNR=?`, [req.query.persnr as string]);
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      db.close();
     }
-    db.close();
-  });
+  })();
 });
+
+// Artikel
+app.get("/")
+
+export const fetchAll = async (db: Database, sql: string, params: Array<string>) => {
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, (err, rows) => {
+      if (err) reject(err);
+      resolve(rows);
+    });
+  });
+};
+
+export const fetchFirst = async (db: Database, sql: string, params: Array<string>) => {
+  return new Promise((resolve, reject) => {
+    db.get(sql, params, (err, row) => {
+      if (err) reject(err);
+      resolve(row);
+    });
+  });
+};
+
 
 
 app.listen(PORT, () => {
