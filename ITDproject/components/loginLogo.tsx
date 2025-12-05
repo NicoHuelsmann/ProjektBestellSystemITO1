3b1b3c18e248e8989f3a64ebed2d16559d542d4b
ChrisBringemeier <164677966+ChrisBringemeier@users.noreply.github.com>
2025-12-05 11:45:38 +0100

    WebLogo

--- ITDproject/components/loginLogo.tsx
+++ ITDproject/components/loginLogo.tsx
@@ -1,5 +1,6 @@
 import {View} from "react-native";
 import {Image} from "expo-image";
+import React from "react";
 
 export function LoginLogo():React.JSX.Element {
     const logoPath = require('../assets/img.png');
